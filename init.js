b831a89f15478353835f7f7da85550809469dec8
Taigo Ito <webmaster@qwel.design>
2025-04-11 17:12:23 +0900

    Start creating my portfolio: QWEL in Action.

--- init.js
+++ init.js
@@ -4,26 +4,42 @@
  * Location: Fukui, Japan
  */
 
+const observer = new IntersectionObserver(([entry]) => {
+  const header = document.getElementById('gNav');
+  header.classList.toggle('gNav--shrink', !entry.isIntersecting);
+});
+
+observer.observe(document.getElementById('sentinel'));
+
+
 // Back To Top
+import BackToTop from './js/_backToTop.js';
 new BackToTop();
 
 // Drawer Menu
+import DrawerMenu from './js/_drawerMenu.js';
 new DrawerMenu();
 
 // Embed
+import Embed from './js/_embed.js';
 new Embed();
 
 // Fader
+import Fader from './js/_fader.js';
 new Fader();
 
 // Evil Icons
 new EvilIcons();
+import EvilIcons from './js/_evilIcons.js';
 
 // Preloader
 new Preloader();
+import Preloader from './js/_preloader.js';
 
 // Responsive Color
 new ResponsiveColor();
+import ResponsiveColor from './js/_responsiveColor.js';
 
 // Slider
 new Slider();
+import Slider from './js/_slider.js';
